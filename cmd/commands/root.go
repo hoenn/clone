fa11ec0f2d4434f4edb101babbcb95448aec7647
hoenn <hoenn@github.com>
2021-10-27 13:14:51 -0400

    Fix a few bugs

--- cmd/commands/root.go
+++ cmd/commands/root.go
@@ -10,11 +10,21 @@ import (
 	giturls "github.com/whilp/git-urls"
 )
 
+var (
+	incHost, dryRun bool
+)
+
+func init() {
+	rootCmd.Flags().BoolVarP(&dryRun, "dry-run", "d", false, "show information but do not actually clone repository")
+	rootCmd.Flags().BoolVarP(&incHost, "include-host", "i", true, "include host in clone structure")
+}
+
 var rootCmd = &cobra.Command{
-	Use:   "clone 'http(s) or git@ URL' [options]",
+	Use:   "clone 'http(s) or git@ URL'",
 	Short: "Clone remote git repositories into a host/owner/repo file structure",
+	Long:  "Clone remote git repositories into a host/owner/repo file structure relative to where this command is run",
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) != 1 {
+		if len(args) < 1 {
 			fmt.Println("expected exactly one URL argument")
 			_ = cmd.Help()
 			os.Exit(1)
@@ -25,6 +35,7 @@ var rootCmd = &cobra.Command{
 			fmt.Printf("Invalid URL '%s': %s\n", rURL, err)
 			os.Exit(1)
 		}
+
 		fullPath := u.RequestURI()
 		if strings.Contains(rURL, "git@") {
 			reqPath := strings.Split(u.RequestURI(), "/")
@@ -32,9 +43,18 @@ var rootCmd = &cobra.Command{
 			fullPath = strings.Trim(fullPath, ".git")
 		}
 		fmt.Println(u.Host)
+		if incHost {
+			fullPath = fmt.Sprintf("/%s%s", u.Host, fullPath)
+		}
+		fmt.Println(fullPath)
 
-		// make clone output relative
 		clonePath := "." + fullPath
+		if dryRun {
+			fmt.Println("Planning to clone:", rURL)
+			fmt.Println("To:", clonePath)
+			os.Exit(0)
+		}
+
 		fmt.Printf("Beginning clone of %s...\n", u.String())
 		_, err = git.PlainClone(clonePath, false, &git.CloneOptions{
 			URL:      rURL,
