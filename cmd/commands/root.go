ba7c679a0465f476280025f15c1c42aa041438db
hoenn <hoenn@github.com>
2021-10-27 12:26:08 -0400

    Accept an arg + help text

--- cmd/commands/root.go
+++ cmd/commands/root.go
@@ -1,10 +1,24 @@
 package commands
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 var rootCmd = &cobra.Command{
+	Use:   "clone 'URL' [options]",
+	Short: "Clone remote git repositories into a host/owner/repo file structure",
 	Run: func(cmd *cobra.Command, args []string) {
-		_ = cmd.Help()
+		if len(args) < 1 {
+			fmt.Println("expected exactly one URL argument")
+			_ = cmd.Help()
+			os.Exit(1)
+		}
+		rURL := args[0]
+		fmt.Printf("Beginning clone of %s...\n", rURL)
+		fmt.Println(rURL)
 	},
 }
 
